e8b321de84e03060df366195c47df1c73d6cb00f
Willem Pol <wpol.edu@gmail.com>
2024-05-16 15:18:08 -0600

    stopping point for user goal / average step layout

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,37 +1,32 @@
 import users from './data/users.js';
-import {getRandomUser, getUserData} from '../src/userFunctions.js'
+import { getRandomUser, getUserData, getAverageStepGoalAllUsers } from '../src/userFunctions.js'
 const allUsers = users.users
-const userMessageInfo = document.querySelector('.welcome-message');
+const welcomeBanner = document.querySelector('.welcome-message');
 const userStepGoalContainer = document.querySelector('.user-step-goal')
 const averageStepContainer = document.querySelector('.average-goal-steps')
+const userStepGoalDisplay = document.querySelector('.display-step-goal')
+const averageStepDisplay = document.querySelector('.display-average-goal-steps')
 const userIdAddressEmail = document.querySelector('.user-id-address-email')
 const userStrideLength = document.querySelector('.user-stride-length')
 const userDailySteps = document.querySelector('.user-daily-step-goal')
 
+const averageStepGoal = getAverageStepGoalAllUsers(allUsers)
 
 window.addEventListener('load', () => {
   updateRandomUserMessage(allUsers);
 });
 
-const retrieveRandomUser = (users) => {
-  const user = getRandomUser(users);
-  return user
-}
-
-const retrieveRandomUserData = (randomUser) => {
-const user = getUserData(allUsers,randomUser.id)
-return user
-}
-
 const displayUserGoal = (user) => {
   userStepGoalDisplay.innerText = `${user.dailyStepGoal}`
 }
 
-const displayAverageSteps = (user) => {
-  averageStepDisplay.innerText = `${}`
-}
+const displayAverageStepGoal = ( (averageStepGoal) => {
+  averageStepDisplay.innerText = `${averageStepGoal}`
+})
 
 const updateRandomUserMessage = (user) => {
+  const randomUser = getRandomUser(users)
+  const user = getUserData(allUsers, randomUser.id)
   updateUserCard(user)
   updateUserMessage(randomUser);
 }
@@ -42,13 +37,13 @@ const updateUserCard = (user) => {
 }
 
 const updateUserMessage = (user) => {  
-  userMessageInfo.innerHTML = `<header>
+  welcomeBanner.innerHTML = `<header>
   <h1 class="welcome-message">Welcome ${user.name}</h1>
   </header>`;
 };
 
 export {
   displayUserGoal,
-  displayAverageSteps,
+  displayAverageStepGoal,
   updateUserMessage
 };
